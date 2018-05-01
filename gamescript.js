05cdec5cadcec0abb41a8e5432b9a02e36affdaa
TomSaporito <t.saporito@gmail.com>
2018-05-01 17:29:29 -0400

    added styles and some js

---
+++ gamescript.js
@@ -0,0 +1,34 @@
+(function(){
+
+    //Game variables
+    var alienX = 80;
+    var alienY = 20;
+
+    var guessX = 0;
+    var guessY = 0;
+
+    var shotsRemaining = 8;
+    var shotsMade = 0;
+
+    var gameState = "";
+    var gameWon = false;
+
+
+    //Game objects
+    var cannon;
+    var alien;
+    var missile;
+    
+
+    //Input Output fields
+    var inputX;
+    var inputY;
+    var output;
+
+
+    //The button;
+    var button;
+
+
+
+})();
